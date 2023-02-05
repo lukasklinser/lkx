abec203c988d3d2e1390dfd990bc1120dc537894
lukas klinser <klinser.lukas@gmail.com>
2023-02-05 20:22:25 +0100

    revert, i know how it works now

--- components/layout.tsx
+++ components/layout.tsx
@@ -25,7 +25,9 @@ export default function Layout({
         />
         <meta
           property="og:image"
-          content={`https://avatars.githubusercontent.com/u/40830132?v=4`}
+          content={`https://og-image.now.sh/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
