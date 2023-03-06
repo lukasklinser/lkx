6174a2f4e16ca4b44eba8e62df0f698e04bfe639
lukas klinser <klinser.lukas@gmail.com>
2023-03-06 11:47:57 +0100

    added audiences

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import { AppProps } from 'next/app';
 import "tailwindcss/tailwind.css";
 import '../styles/tailwind.css'
-import { ThemeProvider } from 'next-themes';
+import { ThemeProvider } from 'next-themes'
+import { Analytics } from '@vercel/analytics/react';
 
 function App({ Component, pageProps }: AppProps) {
   return (
   <ThemeProvider>
     <Component {...pageProps} />
+    <Analytics />
   </ThemeProvider>
   )
 }
